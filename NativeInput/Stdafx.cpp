0875182b131ebc5b7f9f6bce53c89816264f4c92
Alexander <amoiseev@live.com>
2015-12-25 19:16:19 +0300

    native input device

---
+++ NativeInput/Stdafx.cpp
@@ -0,0 +1,5 @@
+// stdafx.cpp : source file that includes just the standard includes
+// NativeInput.pch will be the pre-compiled header
+// stdafx.obj will contain the pre-compiled type information
+
+#include "stdafx.h"
