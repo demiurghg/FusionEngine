a1c90cf21503528218c3ea8dec7ec2b47f912074
demiurghg <demiurghg@gmail.com>
2016-01-21 13:33:35 +0300

    GameLoader

---
+++ Fusion/Engine/Client/GameLoader.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Lidgren.Network;
+using System.Threading;
+using Fusion.Engine.Network;
+using System.IO;
+using Fusion.Engine.Common;
+using Fusion.Core;
+
+
+namespace Fusion.Engine.Client {
+	
+	/// <summary>
+	/// 
+	/// </summary>
+	public abstract class GameLoader : DisposableBase {
+
+		/// <summary>
+		/// 
+		/// </summary>
+		public GameLoader()
+		{
+		}
+
+
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="gameTime"></param>
+		abstract public void Update ( GameTime gameTime );
+	
+	
+		/// <summary>
+		/// Indicates that loader has completed loading process.
+		/// </summary>
+		/// <returns></returns>
+		abstract public bool IsReady {
+			get;
+		}
+	}
+
+}
