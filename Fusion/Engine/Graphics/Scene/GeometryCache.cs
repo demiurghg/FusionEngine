7aae2d3f07e047dea61b6e9e4ad67882e38b7f0f
demiurghg <demiurghg@gmail.com>
2016-05-22 00:36:53 +0300

    Geometry cache

---
+++ Fusion/Engine/Graphics/Scene/GeometryCache.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Runtime.Serialization.Formatters;
+using System.Runtime.Serialization.Formatters.Binary;
+using SharpDX;
+using Fusion;
+using Fusion.Core.Mathematics;
+using Fusion.Drivers.Graphics;
+using Fusion.Core;
+using Fusion.Core.Content;
+
+
+namespace Fusion.Engine.Graphics.Scene {
+	
+	public class GeometryCache : DisposableBase {
+
+		/// <summary>
+		/// Gets total geometry clip length
+		/// </summary>
+		public TimeSpan	Length { 
+			get;
+			private set;
+		}
+
+		/// <summary>
+		/// Gets total number of vertices
+		/// </summary>
+		public int VertexCount {
+			get;
+			private set;
+		}
+
+
+		/// <summary>
+		/// Gets total number of indices
+		/// </summary>
+		public int VertexCount {
+			get;
+			private set;
+		}
+
+
+		/// <summary>
+		/// Gets material references
+		/// </summary>
+		public IEnumerable<MaterialRef> Materials {
+			get;
+			private set;
+		}
+
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="rs"></param>
+		/// <param name="stream"></param>
+		public GeometryCache ( RenderSystem rs, Stream stream )
+		{
+		}
+		
+
+
+	}
+}
